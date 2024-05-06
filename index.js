9508f1fcfb93fce2cd5f5b3b318ca14b25634bc5
JR <jrlacanlale@gmail.com>
2024-05-06 21:51:34 +0000

    set up loading file commands in index, working on executing those commands

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const fs = require('note:fs');
+const path = require('node:path');
 // Require the necessary discord.js classes
 const { Client, Events, GatewayIntentBits } = require('discord.js');
 const { token } = require('./config.json');
@@ -5,6 +7,25 @@ const { token } = require('./config.json');
 // Create a new client instance
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
+client.commands = new Collection();
+
+const foldersPath = path.join(__dirname, 'commands');
+const commandFolders = fs.readdirSync(foldersPath);
+
+for (const folder of commandFolders) {
+    const commandsPath = path.join(foldersPath,folder);
+    const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+    for (const file of commandFiles) {
+        const filePath = path.join(commandsPath,file);
+        const command = require(filePath);
+        // Set a new item in the Collection with the key as the command name and the value as the exported module
+        if ('data' in command && 'execute' in command) {
+            client.commands.set(command.data.name, command);
+        } else {
+            console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+        }
+    }
+}
 // When the client is ready, run this code (only once).
 // The distinction between `client: Client<boolean>` and `readyClient: Client<true>` is important for TypeScript developers.
 // It makes some properties non-nullable.
@@ -14,3 +35,10 @@ client.once(Events.ClientReady, readyClient => {
 
 // Log in to Discord with your client's token
 client.login(token);
+
+// Recieves command interation
+client.on(Events.InteractionCreate, interaction => {
+    // Makes sure that we only respond to the "ChatInputCommandInteraction"
+    if(!interaction.isChatInputCommand()) return;
+	console.log(interaction);
+});
