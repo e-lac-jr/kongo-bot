0cf8945ad08ff76e86cd7f967b39d97716571c0b
JR <jrlacanlale@gmail.com>
2024-05-08 20:29:56 +0000

    added logic for loading events into index and comments for loading command logic

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const path = require('node:path');
 // Require the necessary discord.js classes
 const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
 const { token } = require('./config.json');
-
 // Create a new client instance
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
@@ -11,7 +10,7 @@ client.commands = new Collection();
 
 const foldersPath = path.join(__dirname, 'commands');
 const commandFolders = fs.readdirSync(foldersPath);
-
+// Logic for loading the commands
 for (const folder of commandFolders) {
     const commandsPath = path.join(foldersPath,folder);
     const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
@@ -26,6 +25,18 @@ for (const folder of commandFolders) {
         }
     }
 }
+// Logic for loading the events
+const eventsPath = path.join(__dirname, 'events');
+const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
 
+for (const file of eventFiles) {
+	const filePath = path.join(eventsPath, file);
+	const event = require(filePath);
+	if (event.once) {
+		client.once(event.name, (...args) => event.execute(...args));
+	} else {
+		client.on(event.name, (...args) => event.execute(...args));
+	}
+}
 // Log in to Discord with your client's token
 client.login(token);
