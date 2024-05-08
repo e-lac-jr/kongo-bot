a60a775210ea422e13761892fe31cb0b56391322
JR <jrlacanlale@gmail.com>
2024-05-08 20:22:11 +0000

    added comments to ready.js for clairty

--- events/ready.js
+++ events/ready.js
@@ -1,7 +1,9 @@
 const { Events } = require('discord.js');
 
 module.exports = {
+    // States which property event this file is for
 	name: Events.ClientReady,
+    // Boolean value to specify if event should only run once
 	once: true,
 	execute(client) {
 		console.log(`Ready! Logged in as ${client.user.tag}`);
