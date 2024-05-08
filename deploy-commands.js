f5a810a1e3c344cbd485ba061f515171ec262f3c
JR <jrlacanlale@gmail.com>
2024-05-08 19:27:59 +0000

    added comment to show how to register commands as global

--- deploy-commands.js
+++ deploy-commands.js
@@ -37,6 +37,7 @@ const rest = new REST().setToken(token);
 		// The put method is used to fully refresh all commands in the guild with the current set
 		const data = await rest.put(
 			Routes.applicationGuildCommands(clientId, guildId),
+			// Use Routes.applicationGuildCommands(clientId), to make these commands global
 			{ body: commands },
 		);
 
