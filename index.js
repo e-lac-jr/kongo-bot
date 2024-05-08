a2f695d5f542e266538fbcc92c221bd5e196a6c1
JR <jrlacanlale@gmail.com>
2024-05-08 20:20:11 +0000

    moved event handling from index to individual files

--- index.js
+++ index.js
@@ -26,34 +26,6 @@ for (const folder of commandFolders) {
         }
     }
 }
-// When the client is ready, run this code (only once).
-// The distinction between `client: Client<boolean>` and `readyClient: Client<true>` is important for TypeScript developers.
-// It makes some properties non-nullable.
-client.once(Events.ClientReady, readyClient => {
-	console.log(`Ready! Logged in as ${readyClient.user.tag}`);
-});
 
 // Log in to Discord with your client's token
 client.login(token);
-
-// Recieves command interation
-client.on(Events.InteractionCreate, async interaction => {
-    // Makes sure that we only respond to the "ChatInputCommandInteraction"
-    if(!interaction.isChatInputCommand()) return;
-	const command = interaction.client.commands.get(interaction.commandName);
-    if(!command) {
-        console.error(`No command matching ${interaction.commandName} was found.`);
-        return;
-    }
-
-    try {
-        await command.execute(interaction);
-    } catch (error) {
-        console.error(error);
-        if(interaction.replied || interaction.deferred) {
-            await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true});
-        } else {
-            await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
-        }
-    }
-});
