e92d4251bd4cf54f78844870c50f6bc67dc8d3b4
JR <jrlacanlale@gmail.com>
2024-05-15 15:46:48 +0000

    learning about interaction reply, follow up, and defer

--- commands/utility/ping.js
+++ commands/utility/ping.js
@@ -1,4 +1,5 @@
 const { SlashCommandBuilder } = require('discord.js');
+const wait = require('node:timers/promises').setTimeout;
 
 module.exports = {
 	cooldown: 5,
@@ -7,5 +8,7 @@ module.exports = {
 		.setDescription('Replies with Pong!'),
 	async execute(interaction) {
 		await interaction.reply('Pong!');
+		await wait(4_000);
+		await interaction.followUp({ content: 'Secret Pong!!!', ephemeral: true });
 	},
 };
