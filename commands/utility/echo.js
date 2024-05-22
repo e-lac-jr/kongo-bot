e8da37c88b12fcb11a33f219a436c1a6d77038aa
JR <jrlacanlale@gmail.com>
2024-05-22 16:45:26 +0000

    added max length to echo command

--- commands/utility/echo.js
+++ commands/utility/echo.js
@@ -7,7 +7,8 @@ module.exports = {
 	.addStringOption(option =>
         option.setName('input')
         .setDescription('The input to echo back.')
-        .setRequired(true)),
+        .setRequired(true)
+        .setMaxLength(2_000)),
     async execute(interaction) {
             await interaction.reply(interaction.options.getString('input',true));
     }
