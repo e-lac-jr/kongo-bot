97e806f653ff4a9dbea00b0456b270c298bbe049
JR <jrlacanlale@gmail.com>
2024-05-22 16:41:37 +0000

    added an echo command and changed ineractionCreate.js so that error would be sent to user

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -42,7 +42,7 @@ module.exports = {
 			if (interaction.replied || interaction.deferred) {
 				await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
 			} else {
-				await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+				await interaction.reply({ content: `There was an error while executing this command! Error: ${error}`, ephemeral: true });
 			}
 		}
 	},
